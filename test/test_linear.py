3c67e2919e3dbc031676cccb64bec8cac88c39f5
sofiadorogova <113040802+sofiadorogova@users.noreply.github.com>
2025-03-05 21:29:55 +0300

    tests for linear interpolation

---
+++ test/test_linear.py
@@ -0,0 +1,52 @@
+import pytest
+import numpy as np
+from interpolation_package.interpolation_methods import linear_interpolation
+
+def test_linear_basic():
+    """
+    Проверяем, что на участке [0, 10], где y = x,
+    линейная интерполяция даёт верное значение в середине.
+    """
+    x_vals = np.array([0, 10], dtype=float)
+    y_vals = np.array([0, 10], dtype=float)
+    assert linear_interpolation(x_vals, y_vals, 5) == 5
+
+def test_linear_multiple_points():
+    """
+    Проверяем интерполяцию для массива x_new
+    """
+    x_vals = np.array([0, 10], dtype=float)
+    y_vals = np.array([0, 20], dtype=float)
+    x_new = np.array([2, 4, 6, 8], dtype=float)
+    expected = np.array([4, 8, 12, 16], dtype=float)
+
+    result = linear_interpolation(x_vals, y_vals, x_new)
+    assert np.allclose(result, expected)
+
+def test_linear_out_of_bounds_left():
+    """
+    Если x_new < x_vals[0], наша реализация берёт y = y_vals[0].
+    """
+    x_vals = np.array([2, 4], dtype=float)
+    y_vals = np.array([2, 4], dtype=float)
+    assert linear_interpolation(x_vals, y_vals, 0) == 2
+
+def test_linear_out_of_bounds_right():
+    """
+    Если x_new > x_vals[-1], берём y = y_vals[-1].
+    """
+    x_vals = np.array([2, 4], dtype=float)
+    y_vals = np.array([2, 4], dtype=float)
+    assert linear_interpolation(x_vals, y_vals, 10) == 4
+
+def test_linear_in_the_middle():
+    """
+    Дополнительно проверяем интерполяцию где x_new внутри промежутка,
+    но не ровно посередине.
+    """
+    x_vals = np.array([0, 2], dtype=float)
+    y_vals = np.array([0, 10], dtype=float)
+    # Участок [0,2], y меняется от 0 до 10 => y = 5 * x
+    x_new = 1
+    expected = 5
+    assert linear_interpolation(x_vals, y_vals, x_new) == pytest.approx(expected, 0.0001)
