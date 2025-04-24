e411c640a6f85fe06013c4593ed6ac403098fdde
sofiadorogova <113040802+sofiadorogova@users.noreply.github.com>
2025-04-24 13:34:17 +0300

    Update noxfile.py
    обновление с добавлением документации

--- noxfile.py
+++ noxfile.py
@@ -1,55 +1,38 @@
 import nox
 from nox_poetry import Session, session
 
-# Название пакета
 PACKAGE = "interpolation_package"
 
-# Сессии, которые будут запускаться по умолчанию при вводе "nox"
-nox.options.sessions = ["format", "lint", "tests"]
+nox.options.sessions = ["format", "lint", "tests", "docs"]
 
-# Список файлов, где хотим проверять стиль или форматировать
 LOCATIONS = [
-    "interpolation_package",
-    "test",
+    PACKAGE,
+    "tests",
     "noxfile.py",
 ]
 
+DOCS_LOCATIONS = ["docs/source"]  # Локации для документов
 
 @session(python="3.10")
 def format(session: Session) -> None:
-    """
-    Запуск автоформатирования Ruff для заданных локаций.
-    """
+    """Запуск автоформатирования Ruff."""
     session.install("ruff")
-    # Запуск команды Ruff в режиме форматирования (экспериментальная фича Ruff)
     session.run("ruff", "format", *LOCATIONS)
-
 
 @session(python="3.10")
 def lint(session: Session) -> None:
-    """
-    Запуск Ruff в режиме линтинга (проверка стиля, unused imports и т.д.).
-    """
+    """Проверка стиля кода и импорта."""
     session.install("ruff")
     session.run("ruff", "check", *LOCATIONS)
 
-
 @session(python="3.10")
 def tests(session: Session) -> None:
-    """
-    Запуск тестов через pytest (с установкой зависимостей из Poetry).
-    """
-    # Устанавливаем проект (локально) и dev-зависимости (pytest и т.п.)
-    # nox_poetry автоматически подтянет зависимости из pyproject.toml
-    session.install("pytest", "pytest-cov")
-    session.install(".")  # Установка текущего пакета (.)
+    """Запуск тестов через PyTest."""
+    session.install(".", "pytest", "pytest-cov")
+    session.run("pytest", "--cov", PACKAGE, "--cov-report=term-missing", *session.posargs)
 
-    # Запускаем pytest
-    # --cov=PACKAGE  для покрытия, --cov-report=term-missing выводит непокрытые строки
-    session.run(
-        "pytest",
-        "--cov",
-        "--cov=interpolation_package",
-        "--cov-report=term-missing",
-        *session.posargs,  # Позволяет передавать доп. аргументы, если захотим
-    )
+@session(name="docs", python="3.10")
+def docs(session: Session) -> None:
+    """Сборка документации с помощью Sphinx."""
+    session.chdir("docs/")
+    session.run("sphinx-build", "-b", "html", "./source", "./build")
